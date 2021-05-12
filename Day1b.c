b076290b0a77a7da18b44f4cc2b573efa5de4bcc
albinmanuel <83937205+albinmanuel@users.noreply.github.com>
2021-05-12 12:43:10 +0530

    Create Day1b.c

---
+++ Day1b.c
@@ -0,0 +1,27 @@
+//Write a C Program to perform Read and Print operations using the four basic data types in C. (int, float, char & double).
+
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() 
+{
+  char a; 
+  int b;
+  float c;
+  double d;
+    
+    scanf("%c",&a);
+    printf("%c\n",a);
+    
+    scanf("%d",&b);
+    printf("%d\n",b);
+    
+    scanf("%f",&c);
+    printf("%f\n",c);
+    
+    scanf("%lf",&d);
+    printf("%lf\n",d);
+    return 0;
+}
