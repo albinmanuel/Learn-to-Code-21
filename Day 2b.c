b35a19f72d20d7d87308da0851b24e269381d3a3
albinmanuel <83937205+albinmanuel@users.noreply.github.com>
2021-05-13 12:10:03 +0530

    Create Day 2b.c

---
+++ Day 2b.c
@@ -0,0 +1,18 @@
+//Write a C Program to read an integer and find it's square root.
+
+#include <stdio.h>
+#include <math.h>
+
+int main() 
+{
+    int a;
+
+    scanf("%d",&a);
+
+    printf("%.1f",sqrt(a));
+   
+    return 0;
+}
+
+
+
