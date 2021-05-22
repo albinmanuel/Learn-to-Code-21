333583a76751124073496386b2a3c3cf3fc5eebe
albinmanuel <83937205+albinmanuel@users.noreply.github.com>
2021-05-22 19:25:37 +0530

    Create Day6a.c

---
+++ Day6a.c
@@ -0,0 +1,12 @@
+//WACP to accept marks of three subjects.. Calculate the percentage and respective grade according to following: Percentage >= 90% : Grade A Percentage >= 80% : Grade B Percentage >= 70% : Grade C Percentage >= 60% : Grade D Percentage >= 40% : Grade E Percentage < 40% : Grade F
+
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */    
+    return 0;
+}
