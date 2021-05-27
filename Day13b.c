f9a583eae26df833fbe8d524734c4fa6f3c46fb1
albinmanuel <83937205+albinmanuel@users.noreply.github.com>
2021-05-27 20:15:51 +0530

    Create Day13b.c

---
+++ Day13b.c
@@ -0,0 +1,33 @@
+//Write a C program to change the case of alphabets.
+
+#include <stdio.h>
+#include <string.h>
+
+int main() 
+{
+    char arr[100];
+    int i;
+    
+    fgets(arr,100,stdin);
+    
+    for(i=0;i<strlen(arr);i++)
+    {
+        if(arr[i]>=65 && arr[i]<=90)
+        {
+            arr[i]=arr[i]+32;
+        }
+        
+        else if(arr[i]>=97 && arr[i]<=122)
+        {
+            arr[i]=arr[i]-32;
+        }
+        
+        
+    }
+    
+    printf("%s",arr);
+    
+    
+    
+    return 0;
+}
