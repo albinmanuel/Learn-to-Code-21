8c167da9b08c6c46c4f3577e3e1288b6a3f6c6b8
albinmanuel <83937205+albinmanuel@users.noreply.github.com>
2021-06-06 16:03:30 +0530

    Create Day19b.c

---
+++ Day19b.c
@@ -0,0 +1,28 @@
+//Write a C program to find the number of words in a string. Also reverse string.
+
+#include <stdio.h>
+#include <string.h>
+
+void str(char b[50])
+{
+    int i,c=0;
+    for(i=0; b[i]!='\0'; i++)
+    {
+        if(b[i] == ' ')
+            c++;
+    }
+    printf("no of words = %d",c+1);
+    printf("\n");
+    
+    for(i=strlen(b)-1;i>=0;i--)
+        printf("%c",b[i]);
+}
+
+int main() 
+{
+    char a[50];
+    fgets(a,50,stdin);
+    str(a);
+       
+    return 0;
+}
